e30958758fa332332810d0a3a2c6ea24666f2173
liushuaic <lavedream@hotmail.com>
2015-12-18 16:15:12 +0800

    add 1.添加日期格式化

--- fils/src/main/java/com/yikangyiliao/base/utils/DateUtils.java
+++ fils/src/main/java/com/yikangyiliao/base/utils/DateUtils.java
@@ -1,24 +1,36 @@
 package com.yikangyiliao.base.utils;
 
-import java.io.PrintStream;
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class DateUtils
-{
-  public static Long getＭillisecond(String dateStr)
-    throws ParseException
-  {
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-    return Long.valueOf(sdf.parse(dateStr).getTime());
-  }
+public class DateUtils {
 
-  public static void main(String[] args) throws ParseException {
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-    SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    Date d = sdf.parse("2015-07-15");
-    System.out.println(d.getTime());
-    System.out.println(sdf2.format(sdf.parse("2015-07-15")));
-  }
+	private static String defaultFormatDateStr = "yyyy-MM-dd HH:mm:ss";
+	
+	private static SimpleDateFormat simpleDateFormat;
+
+	public static Long getＭillisecond(String dateStr) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		return Long.valueOf(sdf.parse(dateStr).getTime());
+	}
+
+	/**
+	 * @author liushuaic
+	 * @date 2015/12/12 00:49 
+	 * 格式化日期
+	 * **/
+	public static String formartDateToDateStr(Date date){
+		simpleDateFormat =new SimpleDateFormat(defaultFormatDateStr);
+		return simpleDateFormat.format(date);
+	}
+	
+	public static void main(String[] args) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date d = sdf.parse("2015-07-15");
+		System.out.println(d.getTime());
+		System.out.println(sdf2.format(sdf.parse("2015-07-15")));
+	}
 }
